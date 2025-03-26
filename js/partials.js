90fe8a7e042ba24dd4c145f1d18df80f85280382
emanavas <emanavas@hotmail.com>
2025-03-26 21:35:37 +0100

    commit test folder vercel

--- js/partials.js
+++ js/partials.js
@@ -1,22 +1,54 @@
-import {getFilesInFolder, generateMenu} from "/js/menu_generator.js";
 
-const Partial = {
-    load_into_class: function(class_name, url) {
-        console.log("Cargando " + url);
-        //función local
-        fetch(url) //hace una petición para obtener un arvivo
-            .then((response) => response.text()) //convierte la respuesta a texto
-            .then((data) => (document.getElementsByClassName(class_name).innerHTML = data))
-            .then((data) => console.log(data)) //inserta el texto en el contenedor
-            .catch((error) => console.error(`Error al cargar ${url}:`, error)); //manejo de errores
-    },
-    load_into_id: function(id, url) {
-        //función local
-        fetch(url) //hace una petición para obtener un arvivo
-            .then((response) => response.text()) //convierte la respuesta a texto
-            .then((data) => (document.getElementById(id).innerHTML = data)) //inserta el texto en el contenedor
-            .catch((error) => console.error(`Error al cargar ${url}:`, error)); //manejo de errores
+
+function getFilesInFolder(url, folderPath , filter = ".html") {
+
+    const result = fetch(url+folderPath)
+        .then((response) =>{ 
+            return response.text()
+        })
+        .then(data => {                
+            let el = document.createElement('html');
+            el.innerHTML = data;
+
+            //let list = el.children[1].childNodes[3].childNodes[1].childNodes[3].childNodes[2].childNodes[0].href;
+            let list = el.querySelector("#files")
+            let arr = [];
+            for (let i = 1; i < list.children.length; i++) {
+                const res = list.children[i].querySelectorAll(".name")[0].innerHTML;
+                if( res.endsWith(filter)){
+                    arr.push( folderPath+res ) ;
+                } 
+            }
+            return arr;
+        })
+    return result
+}
+
+function generateMenu(elementList, stringTag) {
+    let menu = '';
+    for (let i = 0; i < elementList.length; i++) {
+        const element = elementList[i];
+        menu += `<${stringTag}><a href="${element}">${stringTag}</a></${stringTag}>`;
     }
+    return menu;
+}
+
+function load_into_class (class_name, url) {
+    console.log("Cargando " + url);
+    //función local
+    fetch(url) //hace una petición para obtener un arvivo
+        .then((response) => response.text()) //convierte la respuesta a texto
+        .then((data) => (document.getElementsByClassName(class_name).innerHTML = data))
+        .then((data) => console.log(data)) //inserta el texto en el contenedor
+        .catch((error) => console.error(`Error al cargar ${url}:`, error)); //manejo de errores
+}
+
+function load_into_id (id, url) {
+    //función local
+    fetch(url) //hace una petición para obtener un arvivo
+        .then((response) => response.text()) //convierte la respuesta a texto
+        .then((data) => (document.getElementById(id).innerHTML = data)) //inserta el texto en el contenedor
+        .catch((error) => console.error(`Error al cargar ${url}:`, error)); //manejo de errores
 }
 
 function load_main_navbar (){
@@ -29,7 +61,8 @@ function generate_script_pages() {
     });
 }
 
-window.load_main_navbar = load_main_navbar;
+window.load_into_class = load_into_class;
+window.load_into_id = load_into_id;
 window.generate_script_pages = generate_script_pages;
 
 
